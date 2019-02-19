6735fe95e480cdb2d6007b31ca1e974f8308ce50
Mohamed Saleh <47383065+Thwellah@users.noreply.github.com>
2019-02-19 14:57:55 +0200

    Mohamed Saleh 34-13613 UPDATED

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const students = [
     { id: "28-09121", name: "Omar Sherif", github_username: "osheriff", email: "omarr@whatever.com" },
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
-    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg",
-   { id: "34-13613", name: "Mohamed Saleh", github_username: "Thwellah", email: "Thwellah@gmail.com" } }
+    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg"},
+   { id: "34-13613", name: "Mohamed Saleh", github_username: "Thwellah", email: "Thwellah@gmail.com" } 
 ];
 
 app.get('/', (request, response) => {
